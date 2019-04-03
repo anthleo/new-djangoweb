669beeb160a3e0829a2d47e7922dbdd2f6592068
anthleo <anthleo@163.com>
2019-04-03 20:44:14 +0800

    add a new bool colunm

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,8 +17,8 @@ class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     create_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
-#    is_deleted = models.BooleanField(default=False)
-#    readed_num = models.IntegerField(default=0)
+    is_deleted = models.BooleanField(default=False)
+    readed_num = models.IntegerField(default=0)
 
     def __str__(self):
         return "<Blog: %s>" %(self.title)
